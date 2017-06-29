6b283481a342c4c2bcc940a33a905242ca15bf54
Pascal Brandt <psbrandt@gmail.com>
2017-06-28 22:04:49 -0700

    🔎 Correct AngularJS details.

--- app/js/home/controllers/patientSearch.controller.js
+++ app/js/home/controllers/patientSearch.controller.js
@@ -4,7 +4,7 @@ class PatientSearchController {
     var vm = this;
     vm.patients = [];
     vm.onsearch = () => {
-      openmrsRest.listFull('patient', {q: '{{vm.query}}', includeAll: true}).then(function (response) {
+      openmrsRest.listFull('patient', {q: vm.query, includeAll: true}).then(function (response) {
         if(response.results.length > 0){
           vm.patients = response.results;
         }
